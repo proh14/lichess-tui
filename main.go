e1aa3d181d388f75ece873cb11a142ce4da88797
kul-sudo <ulyanov.kirill@protonmail.com>
2024-10-11 23:34:14 +0300

    did it

--- main.go
+++ main.go
@@ -26,6 +26,21 @@ func newPrimitive(text string) tview.Primitive {
 	return tview.NewButton(text)
 }
 func main() {
+	theme := tview.Theme{
+		PrimitiveBackgroundColor:    tcell.ColorDefault,
+		ContrastBackgroundColor:     tcell.ColorDefault,
+		MoreContrastBackgroundColor: tcell.ColorDefault,
+		BorderColor:                 tcell.ColorDefault,
+		TitleColor:                  tcell.ColorDefault,
+		GraphicsColor:               tcell.ColorDefault,
+		PrimaryTextColor:            tcell.ColorDefault,
+		SecondaryTextColor:          tcell.ColorDefault,
+		TertiaryTextColor:           tcell.ColorDefault,
+		InverseTextColor:            tcell.ColorDefault,
+		ContrastSecondaryTextColor:  tcell.ColorDefault,
+	}
+	tview.Styles = theme
+
 	// menu := newPrimitive("Menu")
 	// main := newPrimitive("Main content")
 	// sideBar := newPrimitive("Side Bar")
@@ -41,20 +56,7 @@ func main() {
 			grid.AddItem(newPrimitive("0"), i, j, 1, 1, 0, 0, false)
 		}
 	}
-	theme := tview.Theme{
-		PrimitiveBackgroundColor:    tcell.ColorDefault,
-		ContrastBackgroundColor:     tcell.ColorDefault,
-		MoreContrastBackgroundColor: tcell.ColorDefault,
-		BorderColor:                 tcell.ColorDefault,
-		TitleColor:                  tcell.ColorDefault,
-		GraphicsColor:               tcell.ColorDefault,
-		PrimaryTextColor:            tcell.ColorDefault,
-		SecondaryTextColor:          tcell.ColorDefault,
-		TertiaryTextColor:           tcell.ColorDefault,
-		InverseTextColor:            tcell.ColorDefault,
-		ContrastSecondaryTextColor:  tcell.ColorDefault,
-	}
-	tview.Styles = theme
+
 	grid.AddItem(newPrimitive("Surrender"), 8, 0, 1, 8, 0, 0, false)
 	grid.AddItem(newPrimitive("Offer draw"), 9, 0, 1, 8, 0, 0, false)
 	grid.AddItem(newPrimitive("Propose takeback"), 10, 0, 1, 8, 0, 0, false)

85c0e969728ec487c68ad1b9e384912dba09d708
kul-sudo <ulyanov.kirill@protonmail.com>
2025-01-09 22:04:40 +0300

    check if exists

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	// "bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
-	"github.com/go-zoox/fetch"
+	"net/http"
+	"io/ioutil"
 )
 
 type login struct {
@@ -24,18 +27,52 @@ func main() {
 				return errors.New("The token can't be empty.")
 			}
 
-			// fetch.Headers.Set("Authorization", "Bearer " + "lip_EqXU9GUvR4a2bAzFdQM6")
-			// fetch.Headers.Set("Content-Type", "application/json")
-			
-			response, _ := fetch.Get("https://lichess.org/inbox/hoorad123", &fetch.Config{
-				Body: map[string]interface{}{
-					"text": "hello",
-				},
-			})
+			headers := map[string]string{
+				"Authorization": "Bearer " + login.token,
+				"Content-Type":  "application/json",
+			}
 
-			fmt.Println(response.JSON())
+			// Create the request body
+			// body := map[string]string{
+			// 	"text": "hello",
+			// }
 
-			return nil
+			// Serialize the body to JSON
+			// bodyBytes, _ := json.Marshal(body)
+			// if err != nil {
+			// 	log.Fatalf("Error marshalling body: %v", err)
+			// }
+
+			// Make the POST request
+			// url := "https://lichess.org/inbox/hoorad123"
+			url := "https://lichess.org/api/account"
+			// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(bodyBytes))
+			// req, _ := http.NewRequest("POST", url, nil)
+			req, _ := http.NewRequest("GET", url, nil)
+
+			// Set headers
+			for key, value := range headers {
+				req.Header.Set(key, value)
+			}
+
+			// Send the request
+			client := &http.Client{}
+			resp, _ := client.Do(req)
+			defer resp.Body.Close()
+
+			// Read the response body
+			respBody, _ := ioutil.ReadAll(resp.Body)
+
+			var respMap map[string]string
+			json.Unmarshal(respBody, &respMap)
+
+			value, containsKey := respMap["error"]
+
+			if containsKey {
+				return errors.New("No such token found.")
+			} else {
+				return nil
+			}
 		}),
 			huh.NewFilePicker().
 				Description("Where to store the token?").
@@ -52,8 +89,6 @@ func main() {
 				Value(&login.gpgEncryption),
 			huh.NewNote().Description(login.warningMessage),
 		),
-		huh.NewGroup(
-			huh.NewNote().Description("Token doesn't exist")),
 	).WithProgramOptions(tea.WithAltScreen())
 
 	err := form.Run()

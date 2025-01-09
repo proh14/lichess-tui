e2713dc1b1fb918d09d14821056db0046225f4f3
kul-sudo <ulyanov.kirill@protonmail.com>
2025-01-10 01:52:54 +0300

    add encryption logic

--- main.go
+++ main.go
@@ -5,39 +5,41 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/ProtonMail/gopenpgp/v3/crypto"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
-	"net/http"
 	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type login struct {
-	token          string
-	storeLocation  string
-	gpgEncryption  bool
-	warningMessage string
+	token              string
+	tokenStoreLocation string
+	gpgEncryption      bool
+	warningMessage     string
 }
 
 func main() {
-	login := login{token: "", storeLocation: "", gpgEncryption: true, warningMessage: ""}
+	login := login{token: "", tokenStoreLocation: "", gpgEncryption: true, warningMessage: ""}
+
+	pgp := crypto.PGP()
 
 	form := huh.NewForm(
-		huh.NewGroup(huh.NewInput().Description("Token").Value(&login.token).Placeholder("Token").EchoMode(huh.EchoModePassword).Validate(func(value string) error {
-			if value == "" {
+		huh.NewGroup(huh.NewInput().Description("Token").Value(&login.token).Placeholder("Token").Suggestions([]string{"lip_"}).EchoMode(huh.EchoModePassword).Validate(func(value string) error {
+			if len(value) == 0 {
 				return errors.New("The token can't be empty.")
 			}
 
 			headers := map[string]string{
-				"Authorization": "Bearer " + login.token,
+				"Authorization": "Bearer " + value,
 				"Content-Type":  "application/json",
 			}
 
-			// Create the request body
 			// body := map[string]string{
 			// 	"text": "hello",
 			// }
 
-			// Serialize the body to JSON
 			// bodyBytes, _ := json.Marshal(body)
 			// if err != nil {
 			// 	log.Fatalf("Error marshalling body: %v", err)
@@ -50,17 +52,14 @@ func main() {
 			// req, _ := http.NewRequest("POST", url, nil)
 			req, _ := http.NewRequest("GET", url, nil)
 
-			// Set headers
 			for key, value := range headers {
 				req.Header.Set(key, value)
 			}
 
-			// Send the request
 			client := &http.Client{}
 			resp, _ := client.Do(req)
 			defer resp.Body.Close()
 
-			// Read the response body
 			respBody, _ := ioutil.ReadAll(resp.Body)
 
 			var respMap map[string]string
@@ -69,7 +68,7 @@ func main() {
 			value, containsKey := respMap["error"]
 
 			if containsKey {
-				return errors.New("No such token found.")
+				return errors.New("The given token is invalid.")
 			} else {
 				return nil
 			}
@@ -81,12 +80,27 @@ func main() {
 				ShowPermissions(false).
 				ShowHidden(true).
 				CurrentDirectory("/home").
-				Value(&login.storeLocation),
+				Value(&login.tokenStoreLocation),
 			huh.NewConfirm().
-				Title("Encrypt with GPG").
+				Title("Encrypt with PGP").
 				Affirmative("Yes").
 				Negative("No").
-				Value(&login.gpgEncryption),
+				Value(&login.gpgEncryption).Validate(func(value bool) error {
+				file, _ := os.Create(login.tokenStoreLocation + "/token")
+
+				if value {
+					encHandle, _ := pgp.Encryption().Password([]byte("hunter2")).New()
+					pgpMessage, _ := encHandle.Encrypt([]byte("my message"))
+					armored, _ := pgpMessage.ArmorBytes()
+
+					file.WriteString(string(armored))
+				} else {
+					file.WriteString(login.token)
+				}
+
+				return nil
+			}),
+			// Validate(),
 			huh.NewNote().Description(login.warningMessage),
 		),
 	).WithProgramOptions(tea.WithAltScreen())

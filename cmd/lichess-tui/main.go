588bd65d2ccec29be323db52faa5a05d00476e11
kul-sudo <ulyanov.kirill@protonmail.com>
2025-01-20 04:26:50 +0300

    fix

--- cmd/lichess-tui/main.go
+++ cmd/lichess-tui/main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	// "lichess-tui/internal/config"
-	// "lichess-tui/internal/tui"
-	"lichess-tui/internal/requests"
+	"lichess-tui/internal/config"
+	"lichess-tui/internal/tui"
 
-	// tea "github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	respVar := requests.SeekGameResponse{}
-	// cfg := config.GetConfig()
-	requests.SeekGame(requests.SeekGameConfig{Time: 10, Increment: 0}, "lip_NUvDrFiwfjsW0tneprnA", &respVar)
+	// Load the config, if it doesn't exist, create it
+	config.LoadConfig(config.GetConfigPath())
 
-	// // Load the config, if it doesn't exist, create it
-	// config.LoadConfig(config.GetConfigPath())
-	//
-	// p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
-	//
-	// _, err := p.Run()
-	// if err != nil {
-	// 	panic(err)
-	// }
+	p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
+
+	_, err := p.Run()
+	if err != nil {
+		panic(err)
+	}
 }
